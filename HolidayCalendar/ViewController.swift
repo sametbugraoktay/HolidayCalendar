56b289b31148d5787ded071e56a39ed48d9de7c3
samet bugra oktay <sbokty@gmail.com>
2020-09-11 23:38:41 +0300

    Initial Commit

---
+++ HolidayCalendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HolidayCalendar
+//
+//  Created by Samet Bugra Oktay on 11.09.2020.
+//  Copyright © 2020 Samet Bugra Oktay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
